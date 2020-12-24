5ef7ccfbafa87d8d967b945922b710e0f95d9c3f
Chaedooseok <68419018+Chaedooseok@users.noreply.github.com>
2020-12-24 19:25:04 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, {useMemo, useRef, useReducer, useCallback,createContext }  from 'react';
+import produce from 'immer';
+
 import CreateUser from './CreateUser';
 import UserList from './UserList';
-import useInputs from './useInputs';
 
+
+//window.produce = produce;
 function countActiveUsers(users){
 	console.log('활성 사용자 수를 세는 중...');
 	return users.filter(user=>user.active).length;
 }
-
 
 export const UserDispatch  = createContext(null);
 const initialState={
@@ -50,24 +52,35 @@ function reducer(state, action){
         }
       };
     case 'CREATE_USERS':
-      return {
-        inputs: initialState.inputs,
-        users : state.users.concat(action.user)
-      }
+		return produce(state, draft =>{
+			draft.users.push(action.user);
+		} )
+    //   return {
+    //     inputs: initialState.inputs,
+    //     users : state.users.concat(action.user)
+    //   }
     case 'TOGGLE_USER':
-      return{
-        ...state,
-        users:state.users.map(user=>
-          user.id === action.id
-            ? { ...user, active : !user.active}
-            : user
-          )
-      }
+		return produce(state, draft =>{
+			const user = draft.users.find(user => user.id === action.id);
+			user.active = !user.active;
+		})
+    //   return{
+    //     ...state,
+    //     users:state.users.map(user=>
+    //       user.id === action.id
+    //         ? { ...user, active : !user.active}
+    //         : user
+    //       )
+    //   }
     case 'REMOVE_USER':
-      return{
-        ...state,
-        users:state.users.filter(user=> user.id !==action.id)
-      }
+		return produce(state, draft =>{
+			const index = draft.users.findIndex(user => user.id === action.id);
+			draft.users.splice(index, 1);
+		})
+    //   return{
+    //     ...state,
+    //     users:state.users.filter(user=> user.id !==action.id)
+    //   }
     default:
       throw new Error('Unhandled action');
   }
